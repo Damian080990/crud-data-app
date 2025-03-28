1fe3e74c3a4e4049f21ca74932108e372fa0ff48
Damian Matuszak <damian_matuszak@o2.pl>
2025-03-28 17:54:04 +0100

    add render paths to app

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -1,31 +1,34 @@
 import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 //GET
 export const fetchingCoresData = async () => {
-    const { data } = await axios.get(`http://localhost:3000/core`);
+    const { data } = await axios.get(`${API_URL}/core`);
     return data;
 };
 
 //POST
 export const createNewCoreData = async (core) => {
-    const { data } = await axios.post(`http://localhost:3000/core`, core);
+    const { data } = await axios.post(`${API_URL}/core`, core);
     return data;
 };
 
 //DELETE
 export const deleteCoreData = async ({ id }) => {
-    const { data } = await axios.delete(`http://localhost:3000/core/${id}`);
+    const { data } = await axios.delete(`${API_URL}core/${id}`);
     return data;
 };
 
 //GetthisID
 export const fetchCoreById = async (id) => {
-    const { data } = await axios.get(`http://localhost:3000/core/${id}`);
+    const { data } = await axios.get(`${API_URL}/core/${id}`);
     return data;
 };
 
 // PUT
 export const updateCoreData = async ({ id, core }) => {
-    console.log(`Aktualizuję rekord o ID: ${id}`); // Debugowanie
-    const { data } = await axios.put(`http://localhost:3000/core/${id}`, core);
+    console.log(`Aktualizuję rekord o ID: ${id}`);
+    const { data } = await axios.put(`${API_URL}/core/${id}`, core);
     return data;
 };
