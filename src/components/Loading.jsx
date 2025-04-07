37dd0480eabcabf583f67b4e0c8eefd7e6920f60
Damian Matuszak <damian_matuszak@o2.pl>
2025-04-07 20:00:13 +0200

    add favicon and loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ export const Loading = () => {
         <div className="flex items-center justify-center h-screen">
             <p className="text-center text-2xl font-semibold text-gray-700 px-4">
                 This site is powered by hopes, dreams, and a free server.<br />
-                Please hold – I promise it&apos;s loading.
+                Please hold – I promise it&apos;s loading...
             </p>
         </div>
     )
